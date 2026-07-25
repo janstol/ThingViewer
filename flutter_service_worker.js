d749fc71bb4f7f7f4910d99e62d31801f4af0575
janstol <janstol@users.noreply.github.com>
2026-07-25 15:46:08 +0000

    deploy: f4c88c6825dd43d2489c5c017ab4f66acabc5048

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "f675d0730fe3e025bac09988d7f5e697",
+"flutter_bootstrap.js": "b36ee98a77d494c7897d54bf4884e31f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
