2ef90e4960ec250615066b7674f283d14897d011
Jan Štol <jan@stol.dev>
2026-07-25 16:40:42 +0200

    Use verified PKCS12 upload keystore for release signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
                 keyPassword = keystoreProperties["keyPassword"] as String
                 storeFile = file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
+                keystoreProperties.getProperty("storeType")?.let { storeType = it }
             }
         }
     }
